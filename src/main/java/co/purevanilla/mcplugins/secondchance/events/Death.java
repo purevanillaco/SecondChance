a7dc80e944efaf509f17beaeb7294d9ccd6ea7e7
quiquelhappy <quiquelhappy@gmail.com>
2025-01-22 15:41:22 +0100

    1.21 update

--- src/main/java/co/purevanilla/mcplugins/secondchance/events/Death.java
+++ src/main/java/co/purevanilla/mcplugins/secondchance/events/Death.java
@@ -1,6 +1,8 @@
 package co.purevanilla.mcplugins.secondchance.events;
 
 import co.purevanilla.mcplugins.secondchance.Main;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.ChatColor;
 import org.bukkit.EntityEffect;
 import org.bukkit.Sound;
@@ -8,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -15,39 +18,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
-    public class Death implements Listener {
+public class Death implements Listener {
 
-        @EventHandler
-        public void onDamage(EntityDamageEvent e){
+    @EventHandler
+    public void onDeath(EntityDeathEvent e) {
+        if(e.getEntity() instanceof Player player){
+            try {
+                if(!Main.registry.hadSecondChance(player)){
+                    e.setCancelled(true);
+                }
+            } catch (Exception ex) {
+                Main.registry.getPlugin().getLogger().log(Level.WARNING,ex.getMessage());
+            }
+        }
+    }
 
-            if(e.getEntity() instanceof Player){
-                if((((Player) e.getEntity()).getHealth() - e.getFinalDamage()) <= 0){
+    @EventHandler
+    public void onDamage(EntityDamageEvent e){
+        if(e.getEntity() instanceof Player player){
+            if((player.getHealth() - e.getFinalDamage()) <= 0){
+                try {
+                    if(!Main.registry.hadSecondChance(player)){
+                        e.setCancelled(true);
+                        List<PotionEffect> effects = new ArrayList<>();
+                        effects.add(new PotionEffect(PotionEffectType.SATURATION,30*20,10));
+                        effects.add(new PotionEffect(PotionEffectType.REGENERATION,30*20,10));
+                        player.addPotionEffects(effects);
+                        player.playEffect(EntityEffect.TOTEM_RESURRECT);
+                        player.playSound(e.getEntity().getLocation(), Sound.ITEM_TOTEM_USE,1,1);
+                        player.sendMessage(Component.text("You will always remember this as the day you almost caught Captain ")
+                                .color(NamedTextColor.RED)
+                                .append(Component.text(player.getName()).color(NamedTextColor.RED))
+                                .append(Component.text("!").color(NamedTextColor.RED)));
 
-                    try {
-                        if(!Main.registry.hadSecondChance((Player) e.getEntity())){
-                            e.setCancelled(true);
-                            List<PotionEffect> effects = new ArrayList<>();
-                            effects.add(new PotionEffect(PotionEffectType.SATURATION,30*20,10));
-                            effects.add(new PotionEffect(PotionEffectType.HEAL,30*20,10));
-                            ((Player) e.getEntity()).getPlayer().addPotionEffects(effects);
-                            ((Player) e.getEntity()).getPlayer().playEffect(EntityEffect.TOTEM_RESURRECT);
-                            ((Player) e.getEntity()).getPlayer().playSound(e.getEntity().getLocation(), Sound.ITEM_TOTEM_USE,1,1);
-                            ((Player) e.getEntity()).getPlayer().sendMessage(ChatColor.RED + "You will always remember this as the day you almost caught Captain "+((Player) e.getEntity()).getPlayer().getName()+"!");
-                            if(((Player) e.getEntity()).getPlayer().hasPlayedBefore()){
-                                ((Player) e.getEntity()).getPlayer().sendMessage(ChatColor.GRAY + "Jokes Aside: This is the fist time you almost die this week, since we want you to keep playing here, we are giving you a second chance, there is no /back command here for the default rank, so we wanted to be nice and provide you with a free second chance per week. Please, take care!");
-                            } else {
-                                ((Player) e.getEntity()).getPlayer().sendMessage(ChatColor.GRAY + "Jokes Aside: This is the fist time you almost die, since we want you to keep playing here, we are giving you a second (and last!) chance, there is no /back (on death) command here for the default rank, so we wanted to be nice and provide you with a free second chance per week. Please, take care!");
-                            }
-                            Main.registry.markPlayer((Player) e.getEntity());
+                        if (player.hasPlayedBefore()) {
+                            player.sendMessage(Component.text("Jokes Aside: This is the first time you almost die this week, ")
+                                    .color(NamedTextColor.GRAY)
+                                    .append(Component.text("since we want you to keep playing here, we are giving you a second chance. Please, take care!")
+                                            .color(NamedTextColor.GRAY)));
+                        } else {
+                            player.sendMessage(Component.text("Jokes Aside: This is the first time you almost die, ")
+                                    .color(NamedTextColor.GRAY)
+                                    .append(Component.text("since we want you to keep playing here, we are giving you a second (and last!) chance, so we wanted to be nice and provide you with a free second chance per week. Please, take care!")
+                                            .color(NamedTextColor.GRAY)));
                         }
-                    } catch (Exception ex) {
-                        Main.registry.getPlugin().getLogger().log(Level.WARNING,ex.getMessage());
+                        Main.registry.markPlayer((Player) e.getEntity());
                     }
-
+                } catch (Exception ex) {
+                    Main.registry.getPlugin().getLogger().log(Level.WARNING,ex.getMessage());
                 }
             }
-
-
         }
-
+    }
 }
